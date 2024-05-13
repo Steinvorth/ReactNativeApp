b7132ba13a0433330a0d97e2686dae77ed25d45c
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-05-13 12:07:31 -0600

    Show main view if fonts are loaded

--- react_native_jobs/app/_layout.js
+++ react_native_jobs/app/_layout.js
@@ -1,7 +1,29 @@
 import { Stack } from "expo-router"; //links to other navigation pages.
+import { useCallback } from "react";
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync(); //prevents the splash screen from auto hiding.
 
 const Layout = () => {
-    return <Stack/>;
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+        DMMeduim: require('../assets/fonts/DMSans-Medium.ttf'),        
+    });
+
+    //only show main screen if fonts have been loaded.
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack onLayouyt ={onLayoutRootView}/>;
 }
 
 export default Layout;
